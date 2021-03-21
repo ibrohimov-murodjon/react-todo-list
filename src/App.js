435595b1d4e4eed712a877b2b18483bd5aa50ee3
ibrohimov-murodjon <murodjon.ibrohimov.rishton@gmail.com>
2021-03-20 17:10:17 -0700

    codes changed

--- src/App.js
+++ src/App.js
@@ -6,23 +6,43 @@ import TodoList from "./components/TodoList";
 
 class App extends Component {
   state={
-    items:[
-      {id: 1, title:'wale up'},
-      {id: 2, title:'make breakfast'},
-    ],
+    items:[],
     id: uuid(),
     item: '',
     editItem: false,
   }
-  handleChange = (e) => {console.log('handle change')}
-  handleSubmit = (e) => {console.log('handle submit')}
-  clearList = () => {console.log('clear list ')}
-  handleDelete = (id) => {console.log(`handle delete ${id}`)}
-  handleEdit = (id) => {console.log(`edit ${id}`)}
+  handleChange = (e) => {
+    this.setState({
+      item:e.target.value
+    })
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const newItem = {
+      id:this.state.id,
+      title:this.state.item
+    }
+    const updatedItems = [...this.state.items, newItem]
+    this.setState({
+      items:updatedItems,
+      item:'',
+      id:uuid(),
+      editItem:false
+    })
+  }
+  clearList = () => {
+    console.log('clear list ')
+  }
+  handleDelete = (id) => {
+    console.log(`handle delete ${id}`)
+  }
+  handleEdit = (id) => {
+    console.log(`edit ${id}`)
+  }
   render() {
     return(
       <div className="container">
-        <div className="row">
+        <div className="col">
           <div className="col-10 mx-auto col-md-8 mt-5">
             <h3 className='text-capitalize text-center'>to do input</h3>
         </div>
